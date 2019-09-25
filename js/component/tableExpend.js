42b3af6c865f7a8906173af13ecabe6451692eac
fanyajun <fanyajun@pwrd.com>
2019-09-25 17:15:05 +0800

    线上要。/

--- js/component/tableExpend.js
+++ js/component/tableExpend.js
@@ -1,5 +1,5 @@
 const expandRow = function(resolve, reject) {
-    $.get('../../../../components/table-expand.html').then(function(res) { // 注意$.get(url)
+    $.get('./components/table-expand.html').then(function(res) { // 注意$.get(url)
         resolve({ // 这里是构造一个component
             template: res,
             props: {
